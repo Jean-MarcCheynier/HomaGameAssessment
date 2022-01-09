9d2181adb862ed024d04b03228cb4fd3c8d1bbb8
Jean-MarcCheynier <jmcheynier@gmail.com>
2022-01-09 23:06:59 +0100

    Raw

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const dictionaryList: string[] = [
+  'coucou',
+  'hematome',
+  'jeux',
+  'bougie',
+  'telephone',
+  'abcdef',
+  'cesttretretrelong',
+  'abcgef',
+];
+
+const longestWord = (s: string, list = dictionaryList): string => {
+  //if (s.length > 12) throw new Error('s should not exceed 12 letters');
+  if (!/^[a-zA-Z]+$/.test(s)) {
+    throw new Error('s should only contain letters from the latin alphabet');
+  }
+
+  const sortedList = list.sort((a, b) => b.length - a.length);
+
+  for (const word of sortedList) {
+    if (word.length > 12) continue;
+
+    let copy = s;
+    for (let i = 0; i < word.length; i++) {
+      const char = word.charAt(i);
+      if (copy.includes(char)) {
+        copy = copy.replace(char, '');
+      } else {
+        break;
+      }
+      if (i === word.length - 1) {
+        return word;
+      }
+    }
+  }
+  return;
+};
+
+class TaskWordFinder {
+  public longest: string;
+  public longestWordFinder(fileName: string, s: string) {
+    const data = fs.readFileSync(path.resolve(__dirname, fileName), 'utf8');
+    this.longest = longestWord(s, data.split('\n'));
+  }
+}
+
+const main = () => {
+  const taskWordFinder = new TaskWordFinder();
+  console.time('longestWordFinder');
+  taskWordFinder.longestWordFinder('../test.txt', 'optonoceari');
+  console.timeEnd('longestWordFinder');
+  console.log(taskWordFinder.longest);
+};
+
+main();
